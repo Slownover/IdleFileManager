aecb7acafa2f993f4dec96a16c05fda9344e21a3
g_fly <g.flymc@gmail.com>
2026-06-13 23:59:40 +0200

    Delete dashboard.components directory

--- dashboard.components/ide/ide-types.ts
+++
@@ -1,2 +0,0 @@
-export interface IdeFile { name: string; path: string; isDirectory: boolean; isSymlink: boolean; size: number; mode: string; content?: string; }
-export interface IdeTab { id: string; path: string; name: string; isDirty: boolean; }
