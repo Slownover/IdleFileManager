f7a8a4b2f1fa723f2fa247746d2ebf4561a5987f
Slownover <slownover@tuta.io>
2026-06-14 05:09:28 +0000

    fix: fix css import in IdeFileManager.tsx

--- client/components/IdeFileManager.tsx
+++ client/components/IdeFileManager.tsx
@@ -6,7 +6,7 @@ import IdeConsole from './IdeConsole';
 import IdeSettings from './IdeSettings';
 import IdeSearch from './IdeSearch';
 import { IdeTab, IdeFile } from './ide-types';
-import '';
+import '../ide-filemanager.css';
 
 interface Props {
   children?: React.ReactNode;
