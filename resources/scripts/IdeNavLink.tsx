d01a13ff1206503977fb7f4efdb58df41de611fd
Slownover <slownover@tuta.io>
2026-06-14 03:51:32 +0000

    style: fix indentation (prettify)

--- resources/scripts/IdeNavLink.tsx
+++ resources/scripts/IdeNavLink.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
-import { NavLink, useRouteMatch } from 'react-router-dom';
+import React from "react";
+import { NavLink, useRouteMatch } from "react-router-dom";
 
 const IdeNavLink = () => {
-    const match = useRouteMatch<{ id: string }>('/server/:id');
+    const match = useRouteMatch<{ id: string }>("/server/:id");
     if (!match) return null;
 
     return (
-        <NavLink
-            to={`/server/${match.params.id}/ide`}
-            exact
-        >
+        <NavLink to={`/server/${match.params.id}/ide`} exact>
             IDE
         </NavLink>
     );
